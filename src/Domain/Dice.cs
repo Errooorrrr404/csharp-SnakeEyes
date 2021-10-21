79827adf6063fa6c6c5a9090025e503f7a8f7a60
Benjamin BAPPEL <benjamin.bappel@student.hogent.be>
2021-10-21 09:55:01 +0200

    [+] implement Domain folder

---
+++ src/Domain/Dice.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace Domain
+{
+    public class Dice
+    {
+        #region Fields
+        private readonly Random _randomizer = new();
+        #endregion
+
+        #region Properties
+        public int Dots { get; private set; }
+        #endregion
+
+        #region Methods
+        public Dice()
+        {
+            Dots = 6;
+        }
+        public void Roll()
+        {
+            Dots = _randomizer.Next(1, 7);
+        }
+        #endregion
+    }
+}
---
+++ src/Domain/Game.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Domain
+{
+    public class Game
+    {
+        #region Fields
+
+        private readonly List<int> _highscores;
+        private Dice _dice1;
+        private Dice _dice2;
+        #endregion
+
+        #region Properties
+        public int Eye1 { get
+            {
+                return _dice1.Dots;
+            }
+        }
+        public int Eye2
+        {
+            get
+            {
+                return _dice2.Dots;
+            }
+        }
+        public bool HasSnakeEyes { get
+            {
+                return Eye1 == 1 && Eye2 == 1;
+            }
+        }
+        public IReadOnlyList<int> HighScores { get
+            {
+                return _highscores.AsReadOnly();
+            }
+        }
+        public int Total { get; private set; }
+        #endregion
+
+        #region Methods
+        public Game()
+        {
+            Initialize();
+        }
+
+        private void Initialize()
+        {
+            Total = 0;
+            _dice1 = new Dice();
+            _dice2 = new Dice();
+        }
+
+        public void Play()
+        {
+            _dice1.Roll();
+            _dice2.Roll();
+
+            if (HasSnakeEyes)
+            {
+                _highscores.Add(Total);
+                Restart();
+            } else
+            {
+                Total += (Eye1 + Eye2);
+            }
+        }
+
+        public void Restart()
+        {
+            Initialize();
+        }
+        #endregion
+    }
+}
